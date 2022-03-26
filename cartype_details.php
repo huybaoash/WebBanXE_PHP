9f477387a8dcad9922ba141ab9f4d83f24316a7f
Phạm Trần Huy Bảo <46471321+huybaoash@users.noreply.github.com>
2022-03-27 02:36:07 +0700

    carcompany_cartype_Details front-end

--- cartype_details.php
+++ cartype_details.php
@@ -1,49 +1,107 @@
 
-<<style>
-#carcompany_item:hover {
-  background-color: #F0F0F0;
-}
+<style>
+    .menu-list {
+        font-size: 18px;
+    }
+    
+    .menu-item{
+        font-size:18px;
+        padding-left:10px;
+        margin-right:3px;
+        background-color: none;
+            border: 1px solid rgba(0, 0, 0, 0.2);
+            padding: 5px 5px 0px 5px;
+            color: black;
+            margin-bottom: 5px;
+            text-align: left;
+            border-radius: 4px;
+           
+            box-shadow: 0px 2px 0px 2px #f3f3f3;
+        
+    }
+    
+    #menu {
+        margin-left: 0;
+        margin-right: 0;
+    }
+    th {
+        background-color: #a0d1f6;
+    }
+    tr:hover {
+        background-color: #eeeeee;
+    }
 </style>
+
+
+<?php require_once("must_login.php");
+    require_once("check_role_admin.php");
+    require_once("entities/customer.class.php");
+    require_once("entities/account.class.php");
+    require_once("entities/cartype.class.php");
+  
+?>
+
 <?php 
-     require_once("entities/cartype.class.php");
+    $loaixe = CarType::get_cartype(1);
+    if (isset($_GET["MALOAIXE"])){
+
+        $loaixe = CarType::get_cartype($_GET["MALOAIXE"]);
+        $loaixe = reset ($loaixe);
+
+    }
+    else{
+        header("Location: cartype_list_admin.php");
+    }
+
+?>
+
+<?php 
     include_once("header.php");
     
 
     
     include_once("menu.php");
     
-   
-    
 ?>
 
-<?php 
-    $lstLoaiXE = CarType::toPublicList();
+	<div class="container tt">
+		 <h2>Chi Tiết</h2>
 
-?>
-
-
-<div class="container tt" style ="padding-right: 0px;padding-left: 200px;">
-	<div class="col-md-10">
-	            <fieldset>
-                    <?php 
-                        foreach ($lstLoaiXE as $loaixe) {?>
-
-                            <a href ="contract_list.php?&MALOAIXE=<?php echo $loaixe["MALOAIXE"]; ?>">
-                                <div id = "carcompany_item" class=" col-lg-4 col-md-4 col-sm-6 col-xs-12 " style = "border: 0.5pt solid black;height:180px">
-                                    <div style = "text-align: center;">
-                                        <img src="<?php echo $loaixe["HINHANH"]; ?>" alt="<?php echo $loaixe["TENLOAIXE"]; ?>" style="width:140px;height:140px">
-                                    </div>
-                                    
-                                    <div style = "text-align: center;margin-top:10px  ">
-                                        <label><?php echo $loaixe["TENLOAIXE"]; ?></label>
-                                    </div>
-                                </div>
-                            </a>
-
-                    <?php     }
-                    ?>
-	            </fieldset>
+		<div>
+		    <h4>Loại xe</h4>
+		    <hr />
+		    <dl class="dl-horizontal">
+		        <dt>
+		        	<label>Tên loại xe</label>
+		            
+		        </dt>
+		
+		        <dd>
+                <?php echo $loaixe["TENLOAIXE"] ?>
+		        </dd>
+		        <hr />
+		        <dt>
+		            <label>Hình ảnh</label>
+		        </dt>
+		
+		        <dd>
+		            <img src ="<?php echo $loaixe["HINHANH"] ?>" style="height: 240px;width:240px">
+		        </dd>
+		        <hr />
+		        <dt>
+		            <label>Trạng thái</label>
+		        </dt>
+		
+		        <dd>
+		            <?php echo $loaixe["TRANGTHAI"] ?>
+		        </dd>
+		        <hr />
+		    </dl>
+		</div>
+		<p>
+			<a href="cartype_edit.php?&MALOAIXE=<?php echo $loaixe["MALOAIXE"] ?>" class="btn btn-primary btn-xs">Sửa</a>
+		  	<a href="cartype_list_admin.php" class="btn btn-primary btn-xs"> Trở lại danh sách </a>   
+		</p>
+		
 	</div>
-</div>
-
-<?php include_once("footer.php"); ?>
+    <?php include_once("footer.php"); ?>
