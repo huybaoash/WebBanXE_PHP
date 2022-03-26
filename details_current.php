73e92aa57dabf7d9c63ef44af0329f9b252035cf
Phạm Trần Huy Bảo <46471321+huybaoash@users.noreply.github.com>
2022-03-26 14:12:13 +0700

    Edit_UserInfo

--- details_current.php
+++ details_current.php
@@ -39,7 +39,7 @@
                             
                             <a href="">Số xe đã đăng: </a>
                            
-                                <div class="edit-tt"> <a href="${pageContext.request.contextPath}/userinfo-edit"><i class="far fa-edit"></i> Chỉnh sửa thông tin</a></div>
+                                <div class="edit-tt"> <a href="edit_account_current.php"><i class="far fa-edit"></i> Chỉnh sửa thông tin</a></div>
                                 <div class="edit-tt"> <a href=""><i class="far fa-edit"></i> Danh sách xe đăng ký bán</a></div>
                               
 
